ab27afee039866fe4fa0627f6b80c93a8b239424
ro-lucero <ro_lucero@hotmail.com>
2021-09-02 21:17:00 -0300

    first commit

---
+++ src/Interfaz/Interfaz.java
@@ -0,0 +1,15 @@
+
+package Interfaz;
+
+
+import java.util.Scanner;
+
+public class Interfaz {
+
+   
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+    }
+
+}
